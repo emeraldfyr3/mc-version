6c1169ccc9164f68188bcfcc4a09b5d752f9c8c7
emeraldfyr3 <66810306+emeraldfyr3@users.noreply.github.com>
2021-01-01 18:39:48 -0500

    combine data version commands

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,20 +9,39 @@ DATAVERSION_FUNC_FILE='../data/mcversion/functions/dataversion.mcfunction'
 
 # Generate dataversion.mcfunction from dataversion.csv
 
-echo 'execute store result score #dataversion mcversion run data get entity @p DataVersion
-' > "$DATAVERSION_FUNC_FILE"
+echo "\
+##### GENERATED FILE -- DO NOT EDIT #####
 
-sed '
-  1d
-  s/,/,#/g
-  s/$/,/
-  s/#\([0-9]\),/#0\1,/g
-  s/,#/,/g
-  s/^[^,]*,\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\),/execute if score #dataversion mcversion matches \1 run scoreboard players set #version mcversion \2\3\4/
-  s/ 0*\([0-9][0-9]*\)/ \1/g
-' "$DATAVERSION_CSV_FILE" | tac >> "$DATAVERSION_FUNC_FILE"
+# Determine the version from the player's DataVersion tag
+# Thanks to Evtema3 for pointing this out to me
 
-echo '
+execute store result score #dataversion mcversion run data get entity @p DataVersion
+
+$(
+  minDataVersion=
+  maxDataVersion=
+  prevMcVersion=
+
+  while read line
+  do
+    dataVersion="$(cut -d ',' -f 2 <<< "$line")"
+    mcVersion="$(cut -d ',' -f 3-5 <<< "$line" | sed 's/,/,#/g;s/$/,/;s/#\([0-9]\),/#0\1,/g;s/,#*//g')"
+
+    if [ "$prevMcVersion" ] && [ ! "$prevMcVersion" = "$mcVersion" ]
+    then
+      echo "execute if score #dataversion mcversion matches ${minDataVersion}..${maxDataVersion} run scoreboard players set #version mcversion ${prevMcVersion}"
+
+      minDataVersion="$dataVersion"
+    fi
+
+    [ "$minDataVersion" ] || minDataVersion="$dataVersion"
+    maxDataVersion="$dataVersion"
+    prevMcVersion="$mcVersion"
+  done <<< "$(tail -n +2 "$DATAVERSION_CSV_FILE" | tac)"
+
+  echo "execute if score #dataversion mcversion matches ${minDataVersion}..${maxDataVersion} run scoreboard players set #version mcversion ${prevMcVersion}"
+)
+
 scoreboard players set #100 mcversion 100
 
 scoreboard players operation patch mcversion = #version mcversion
@@ -31,4 +50,4 @@ scoreboard players operation patch mcversion %= #100 mcversion
 execute store result score minor mcversion run scoreboard players operation #version mcversion /= #100 mcversion
 scoreboard players operation minor mcversion %= #100 mcversion
 
-execute store result score major mcversion run scoreboard players operation #version mcversion /= #100 mcversion' >> "$DATAVERSION_FUNC_FILE"
+execute store result score major mcversion run scoreboard players operation #version mcversion /= #100 mcversion" > "$DATAVERSION_FUNC_FILE"
